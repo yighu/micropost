fa8c16b168b1a00abfaf08369800036c74dae237
Yiguang Hu <yiguang.hu@gmail.com>
2011-12-29 14:41:59 -0500

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,9 +45,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+	      flash[:success]="Welcome ....."
         format.html { redirect_to @user, :notice => 'User was successfully created.' }
         format.json { render :json => @user, :status => :created, :location => @user }
       else
+	      @title="Sign up"
         format.html { render :action => "new" }
         format.json { render :json => @user.errors, :status => :unprocessable_entity }
       end
