2d87d828ec520619383c50f4ce42344890d572bd
Yiguang Hu <yiguang.hu@gmail.com>
2011-12-29 16:22:12 -0500

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Micropost::Application.routes.draw do
+  get "sessions/new"
+
   resources :mcroposts
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+match '/signin',  :to => 'sessions#new'
+match '/signout', :to => 'sessions#destroy'
 match '/contact', :to => 'pages#contact'
 match '/about',   :to => 'pages#about'
 match '/help',    :to => 'pages#help'
